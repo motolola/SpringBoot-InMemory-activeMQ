aac0f613141e4688bcb2a594335cb3ad4e8afcd5
motolola <motolola@icloud.com>
2018-08-28 21:46:58 +0100

    second commit

---
+++ src/main/java/io/motolola/inMemoryactiveMQ/InMemoryActiveMqApplication.java
@@ -0,0 +1,12 @@
+package io.motolola.inMemoryactiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryActiveMqApplication.class, args);
+	}
+}
